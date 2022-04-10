f40776034c76d18b40d0e47c0384a4989c14f8eb
Baiqing Lyu <baiqing@bu.edu>
2022-04-10 15:25:39 -0400

    Finish random map generation

--- RPG/World/LegendsOfValorMap.java
+++ RPG/World/LegendsOfValorMap.java
@@ -15,23 +15,48 @@ enum CellType {
 public class LegendsOfValorMap extends Randomness implements Map {
 
     // Create 2d array of tiles that is 8 by 8
-    private static Tile[][] gameContent = new Tile[8][8];
+    private static final Tile[][] gameContent = new Tile[8][8];
+    private final CellType[][] map = new CellType[8][8];
 
     public LegendsOfValorMap() {
-        // Create the map
+        // Create the content map
         int heroIdx = 1;
         for (int i = 0; i < gameContent.length; i++) {
             for (int j = 0; j < gameContent[i].length; j++) {
                 if (i == 0 && (j - 1) % 3 == 0) {
                     gameContent[i][j] = new Tile("enemy");
-                }
-                else if (i == gameContent.length - 1 && (j - 1) % 3 == 0) {
+                } else if (i == gameContent.length - 1 && (j - 1) % 3 == 0) {
                     gameContent[i][j] = new Tile("H" + heroIdx);
                     heroIdx++;
-                }
-                else {
+                } else {
                     gameContent[i][j] = new Tile("wild");
                 }
+            }
+        }
+
+        // Create the cell type map
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                // Nexus cells
+                if ((i == 0 || i == map.length - 1) && (j + 1) % 3 != 0) {
+                    map[i][j] = CellType.NEXUS;
+                }
+                // Other types of cells, 40% chance of being plain and 20% chance being every other type
+                else if ((j + 1) % 3 != 0) {
+                    int rand = getRandomNumber(0, 100);
+                    if (rand < 40) {
+                        map[i][j] = CellType.PLAIN;
+                    } else if (rand < 60) {
+                        map[i][j] = CellType.KOULOU;
+                    } else if (rand < 80) {
+                        map[i][j] = CellType.CAVE;
+                    } else {
+                        map[i][j] = CellType.BUSH;
+                    }
+                } else {
+                    map[i][j] = CellType.INACCESSIBLE;
+                }
+
             }
         }
     }
@@ -80,16 +105,6 @@ public class LegendsOfValorMap extends Randomness implements Map {
 
     @Override
     public void renderMap() {
-        CellType[][] map = {
-                {CellType.NEXUS, CellType.NEXUS, CellType.INACCESSIBLE, CellType.NEXUS, CellType.NEXUS, CellType.INACCESSIBLE, CellType.NEXUS, CellType.NEXUS},
-                {CellType.PLAIN, CellType.PLAIN, CellType.INACCESSIBLE, CellType.CAVE, CellType.PLAIN, CellType.INACCESSIBLE, CellType.BUSH, CellType.BUSH},
-                {CellType.PLAIN, CellType.PLAIN, CellType.INACCESSIBLE, CellType.PLAIN, CellType.PLAIN, CellType.INACCESSIBLE, CellType.PLAIN, CellType.PLAIN},
-                {CellType.CAVE, CellType.BUSH, CellType.INACCESSIBLE, CellType.BUSH, CellType.KOULOU, CellType.INACCESSIBLE, CellType.KOULOU, CellType.PLAIN},
-                {CellType.PLAIN, CellType.PLAIN, CellType.INACCESSIBLE, CellType.BUSH, CellType.PLAIN, CellType.INACCESSIBLE, CellType.PLAIN, CellType.BUSH},
-                {CellType.KOULOU, CellType.KOULOU, CellType.INACCESSIBLE, CellType.KOULOU, CellType.PLAIN, CellType.INACCESSIBLE, CellType.PLAIN, CellType.PLAIN},
-                {CellType.PLAIN, CellType.PLAIN, CellType.INACCESSIBLE, CellType.PLAIN, CellType.PLAIN, CellType.INACCESSIBLE, CellType.PLAIN, CellType.PLAIN},
-                {CellType.NEXUS, CellType.NEXUS, CellType.INACCESSIBLE, CellType.NEXUS, CellType.NEXUS, CellType.INACCESSIBLE, CellType.NEXUS, CellType.NEXUS}
-        };
 
         List<StringBuilder> printableMap = new ArrayList<>();
         int size = 8;
